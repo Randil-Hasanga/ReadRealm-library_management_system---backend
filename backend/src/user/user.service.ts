cdaf4261dc412f35375be5ec2ef203303728cbaf
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2025-03-25 17:22:05 +0530

    Update user.service.ts

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -4,23 +4,23 @@ import { UserDto } from './user.dto';
 
 @Injectable()
 export class UserService {
-    async getUsers()  {
+    
+    async getUsers() {
         const users = await User.findAll();
         return users;
     }
-    async createUser(data : any) {
+
+    async createUser(data: any) {
         const { email } = data;
-
         const existingUser = await User.findOne({ where: { email } });
-
         if (existingUser) {
             console.error('Email is already exist');
         }
-
         const newUser = await User.create(data);
         return newUser;
     }
-    async getUserById (id : number) {
+
+    async getUserById(id: number) {
         const user = await User.findOne({ where: { user_id: id } });
         if (!user) {
             console.error('User not found');
