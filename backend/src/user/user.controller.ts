c27d5cacf1d644a69f2558330f0a41ec2e9d630c
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2025-03-25 17:09:47 +0530

    getUserById

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, Res, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Req, Res, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './user.dto';
 
@@ -17,12 +17,22 @@ export class UserController {
     }
 
     @Post()
-    @HttpCode(HttpStatus.CREATED)
     @UsePipes(new ValidationPipe({ whitelist: true, transform: true }))
     async createUser(@Body() userData: UserDto) {
         return {
             message: 'User Created Successfully',
             data: await this.userService.createUser(userData)
+        }
+    }
+
+    @Get(':id')
+    async getUserById (@Param('id') user_id, @Res() res) {
+        
+        try {
+            const user = await this.userService.getUserById(user_id);
+            res.status(200).json({ message: 'User fetched successfully', data: user });
+        } catch (error) {
+            res.status(500).json({ message: 'Error fetching user', error: error.message });
         }
     }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import User from 'src/models/User';
+import { UserDto } from './user.dto';
 
 @Injectable()
 export class UserService {
@@ -7,7 +8,7 @@ export class UserService {
         const users = await User.findAll();
         return users;
     }
-    async createUser(data) {
+    async createUser(data : any) {
         const { email } = data;
 
         const existingUser = await User.findOne({ where: { email } });
@@ -18,5 +19,12 @@ export class UserService {
 
         const newUser = await User.create(data);
         return newUser;
+    }
+    async getUserById (id : number) {
+        const user = await User.findOne({ where: { user_id: id } });
+        if (!user) {
+            console.error('User not found');
+        }
+        return user;
     }
 }
