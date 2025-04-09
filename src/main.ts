0bb6231ca876491bc6bdc71de3c7b2834f2ac71e
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2025-04-09 19:37:32 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     credentials: true
   });
   app.use(cookieParser());
