c653ddf38ba716fe8cbc6de270a00755bfb970d6
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2025-04-10 19:25:34 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://readrealm-frontend-3da2a969ac8f.herokuapp.com',
     credentials: true
   });
   app.use(cookieParser());
