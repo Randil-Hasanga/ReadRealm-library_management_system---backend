6d69d749b4ff762de84fdc6d198e3c6339e4db15
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2025-04-10 21:57:13 +0530

    Update db.ts

--- db.ts
+++ db.ts
@@ -5,13 +5,19 @@ import * as path from 'path';
 
 dotenv.config();
 
+const certificatePath = process.env.NODE_ENV === 'production'
+    ? path.join(__dirname, '..', 'certificates', 'DigiCertGlobalRootCA.crt')  // For production, it's in the root
+    : path.join(__dirname, 'certificates', 'DigiCertGlobalRootCA.crt');  // For development, it's in src
+
+const sslOptions = process.env.NODE_ENV === 'production' 
+    ? { ca: fs.readFileSync(certificatePath) } 
+    : {};
+
 const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASS as string, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     dialectOptions: {
-        ssl: {
-            ca: fs.readFileSync(path.join(__dirname, 'src','certificates', 'DigiCertGlobalRootCA.crt')),
-        }
+        ssl: sslOptions
     }
 });
 
